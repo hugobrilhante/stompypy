bc0a7cc8c7dcd793aae47446adc1ba1f31efefe4
Hugo Brilhante <hugobrilhante@gmail.com>
2024-02-20 17:26:27 -0300

    Add manager events

---
+++ src/stomppy/managers.py
@@ -0,0 +1,58 @@
+from typing import List
+
+from stomppy.listeners import Listener
+
+
+class EventManager:
+    """
+    A simple event manager to handle subscriptions and notifications.
+
+    Attributes:
+        listeners (List[Listener]): A list to store subscribed listeners.
+    """
+
+    def __init__(self):
+        """
+        Initializes the EventManager with an empty list of listeners.
+        """
+        self.listeners: List[Listener] = []
+
+    def subscribe(self, listener):
+        """
+        Subscribe a listener to the event manager.
+
+        Args:
+            listener: An object that implements the desired event methods.
+
+        Returns:
+            None
+        """
+        self.listeners.append(listener)
+
+    def unsubscribe(self, listener):
+        """
+        Unsubscribe a listener from the event manager.
+
+        Args:
+            listener: An object that was previously subscribed.
+
+        Returns:
+            None
+        """
+        self.listeners.remove(listener)
+
+    def notify(self, event, *args, **kwargs):
+        """
+        Notify all subscribed listeners of an event.
+
+        Args:
+            event: A string representing the event to be triggered.
+            *args: Additional positional arguments to be passed to listeners.
+            **kwargs: Additional keyword arguments to be passed to listeners.
+
+        Returns:
+            None
+        """
+        for listener in self.listeners:
+            if hasattr(listener, event):
+                getattr(listener, event)(*args, **kwargs)
---
+++ tests/unit/test_manager.py
@@ -0,0 +1,39 @@
+from stomppy.managers import EventManager
+
+
+class MockListener:
+    def __init__(self):
+        self.events_triggered = []
+
+    def on_event(self, *args, **kwargs):
+        self.events_triggered.append(('on_event', args, kwargs))
+
+
+def test_subscribe():
+    event_manager = EventManager()
+    listener = MockListener()
+
+    event_manager.subscribe(listener)
+
+    assert listener in event_manager.listeners
+
+
+def test_unsubscribe():
+    event_manager = EventManager()
+    listener = MockListener()
+    event_manager.subscribe(listener)
+
+    event_manager.unsubscribe(listener)
+
+    assert listener not in event_manager.listeners
+
+
+def test_notify():
+    event_manager = EventManager()
+    listener = MockListener()
+    event_manager.subscribe(listener)
+
+    event_manager.notify('on_event', 'arg1', kwarg1='value1')
+
+    assert len(listener.events_triggered) == 1
+    assert listener.events_triggered[0] == ('on_event', ('arg1',), {'kwarg1': 'value1'})
