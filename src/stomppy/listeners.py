81941305c5949d74fcfa11b43719e1c713fcbe77
Hugo Brilhante <hugobrilhante@gmail.com>
2024-02-20 17:27:46 -0300

    Add listener interface

---
+++ src/stomppy/listeners.py
@@ -0,0 +1,46 @@
+from stomppy.frame import Frame
+
+
+class Listener:
+    """
+    Interface for STOMP frame listeners.
+    """
+
+    def on_disconnect(self) -> None:
+        """
+        Invoked when a DISCONNECT frame is sent.
+        """
+        pass
+
+    def on_connect(self) -> None:
+        """
+        Invoked when a CONNECT frame is sent.
+        """
+        pass
+
+    def on_connected(self, frame: Frame) -> None:
+        """
+        Invoked when a CONNECTED frame is received.
+
+        Args:
+            frame (Frame): The CONNECTED frame.
+        """
+        pass
+
+    def on_message(self, frame: Frame) -> None:
+        """
+        Invoked when a MESSAGE frame is received.
+
+        Args:
+            frame (Frame): The MESSAGE frame.
+        """
+        pass
+
+    def on_error(self, frame: Frame) -> None:
+        """
+        Invoked when an ERROR frame is received.
+
+        Args:
+            frame (Frame): the ERROR frame.
+        """
+        pass
