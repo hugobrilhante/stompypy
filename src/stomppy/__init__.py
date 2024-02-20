283685194540026e7c702bc52edacb61622bd4f4
Hugo Brilhante <hugobrilhante@gmail.com>
2024-02-20 19:03:46 -0300

    Bump version 0.3.0

--- src/stomppy/__init__.py
+++ src/stomppy/__init__.py
@@ -1,10 +1,11 @@
-__version__ = '0.1.0'
+import importlib.metadata
+
+__version__ = importlib.metadata.version('stomppy')
 
 from .listeners import Listener
 from .logging_config import setup_logging
 from .stomp import Stomp
 
 setup_logging()
-
 
 __all__ = ['Stomp', 'Listener']
